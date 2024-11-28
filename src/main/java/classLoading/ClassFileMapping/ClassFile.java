85e7101fc2d9526e0629071b7a9df9cb3a29adf4
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-11-28 14:05:53 +0200

    testing

---
+++ src/main/java/classLoading/ClassFileMapping/ClassFile.java
@@ -0,0 +1,5 @@
+package main.java.classLoading.ClassFileMapping;
+
+public class ClassFile {
+    
+}
---
+++ src/main/java/classLoading/Test/Test.java
@@ -0,0 +1,13 @@
+package main.java.classLoading.Test;
+
+public class Test {
+    public static void main(String[] args) {
+        MyClass[] a = new MyClass[5];
+        System.out.println(a[0].x);
+    }
+
+
+}
+class MyClass{
+    static int x = 10 ;
+}
