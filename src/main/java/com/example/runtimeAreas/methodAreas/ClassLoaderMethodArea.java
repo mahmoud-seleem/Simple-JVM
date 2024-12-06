71f0f4ff563ee328ff37d33988c70a0916a8b741
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-12-06 23:22:41 +0200

    adding functionality to remove class info from the method area

--- src/main/java/com/example/runtimeAreas/methodAreas/ClassLoaderMethodArea.java
+++ src/main/java/com/example/runtimeAreas/methodAreas/ClassLoaderMethodArea.java
@@ -3,14 +3,18 @@ package com.example.runtimeAreas.methodAreas;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.bcel.classfile.JavaClass;
 
 import com.example.classLoader.JavaClassInfo;
 
 public class ClassLoaderMethodArea {
     private final Map<String,JavaClassInfo> methodArea = new HashMap<>();
+    
     public void addClass(String className,JavaClassInfo clz){
         methodArea.put(className, clz);
+    }
+
+    public JavaClassInfo removeClass(String className){
+        return methodArea.remove(className);
     }
     public JavaClassInfo getClass(String className){
         return methodArea.get(className);
@@ -18,4 +22,5 @@ public class ClassLoaderMethodArea {
     public Object[] getAllClassesNames(){
         return methodArea.keySet().toArray();
     }
+
 }
