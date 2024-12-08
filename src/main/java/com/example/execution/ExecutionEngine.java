a27fd5dc258d3714f39110fb8c64fa8b1f4b2cb4
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-12-08 13:20:02 +0200

    implementing till 60 instructions set for ints only for now

--- src/main/java/com/example/execution/ExecutionEngine.java
+++ src/main/java/com/example/execution/ExecutionEngine.java
@@ -8,46 +8,100 @@ import com.example.utils.Printing;
 import static com.example.execution.InstructionsLibrary.*;
 
 public class ExecutionEngine {
-    public static void executeMethod(JavaThread thread,Method method) {
-            byte[] byteCode = method.getCode().getCode();
-            for (int i = 0 ; i < byteCode.length ;i++){
-                switch (byteCode[i]) {
-                    case 3:
-                        iconst_0(thread.getCurrentFrame());
-                        break;
-                    default:
-                        break;
-                }
-                Printing.printArray(thread.getCurrentFrame().getOperandStack().toArray());
+    public static void executeMethod(JavaThread thread, Method method) {
+        byte[] byteCode = method.getCode().getCode();
+        StackFrame currentFrame = thread.getCurrentFrame();
+        for (int i = 0; i < byteCode.length; i++) {
+            switch (byteCode[i]) {
+                case 1:
+                    // code for pushing null into the stack.
+                    break;
+                case 2:
+                    iconst_m1(currentFrame);
+                    break;
+                case 3:
+                    iconst_0(currentFrame);
+                    break;
+                case 4:
+                    iconst_1(currentFrame);
+                    break;
+                case 5:
+                    iconst_2(currentFrame);
+                    break;
+                case 6:
+                    iconst_3(currentFrame);
+                    break;
+                case 7:
+                    iconst_4(currentFrame);
+                    break;
+                case 8:
+                    iconst_5(currentFrame);
+                    break;
+                case 21:
+                    iload(byteCode[i + 1], currentFrame);
+                    i++;
+                    break;
+                case 26:
+                    iload_0(currentFrame);
+                    break;
+                case 27:
+                    iload_1(currentFrame);
+                    break;
+                case 28:
+                    iload_2(currentFrame);
+                    break;
+                case 29:
+                    iload_3(currentFrame);
+                    break;
+                case 54:
+                    istore(byteCode[i + 1], currentFrame);
+                    i++;
+                    break;
+                case 59:
+                    istore_0(currentFrame);
+                    break;
+                case 60:
+                    istore_1(currentFrame);
+                    break;
+                case 61:
+                    istore_2(currentFrame);
+                    break;
+                case 62:
+                    istore_3(currentFrame);
+                    break;
+                default:
+                    break;
             }
+        }
+        Printing.printArray(thread.getCurrentFrame().getOperandStack().toArray());
     }
 
     // method for creating the frame to a new method and pass the args to it.
-    public static void callMethod(JavaThread thread ,Method method,Object[] args){
-        
+    public static void callMethod(JavaThread thread, Method method, Object[] args) {
+
         // creation of the stack for the new method in the same thread.
         StackFrame currentFrame = thread.createStackFrame(method
-            .getLocalVariableTable()
-            .getTableLength());
+                .getLocalVariableTable()
+                .getTableLength());
 
         // passing args to it.
-        if(method.getName().equals("main")){
+        if (method.getName().equals("main")) {
             passArgs(args, currentFrame.getLocals(), true);
-        }else{
-            // code for any static method passing args 
+        } else {
+            // code for any static method passing args
         }
         Printing.printArray(currentFrame.getLocals());
     }
 
-    private static void passArgs(Object[] callerArgs,Object[] calledLocals,boolean isStatic){
-        int index ;
-        if (isStatic){
+    private static void passArgs(Object[] callerArgs, Object[] calledLocals, boolean isStatic) {
+        int index;
+        if (isStatic) {
             index = 0;
-        }else{
+        } else {
             index = 1;
-        } 
-        for (Object object : callerArgs){
-         calledLocals[index] = object;   
+        }
+        for (Object object : callerArgs) {
+            calledLocals[index] = object;
         }
     }
 }
--- src/main/java/com/example/execution/InstructionsLibrary.java
+++ src/main/java/com/example/execution/InstructionsLibrary.java
@@ -1,10 +1,74 @@
 package com.example.execution;
 
+import org.apache.bcel.classfile.Method;
+
+import com.example.runtimeAreas.threads.JavaThread;
 import com.example.runtimeAreas.threads.StackFrame;
 
 public class InstructionsLibrary {
-    // pushing value to stack 
-    public static void iconst_0(StackFrame frame){
-        frame.push((int)0);
+    // pushing value to stack
+    public static void interpretByeCode(byte byteCode, JavaThread thread, Method method) {
+        StackFrame currentFrame = thread.getCurrentFrame();
+        
     }
+
+    public static void iconst_0(StackFrame frame) {
+        frame.push((int) 0);
+    }
+
+    public static void iconst_1(StackFrame frame) {
+        frame.push((int) 1);
+    }
+
+    public static void iconst_2(StackFrame frame) {
+        frame.push((int) 2);
+    }
+
+    public static void iconst_3(StackFrame frame) {
+        frame.push((int) 3);
+    }
+
+    public static void iconst_4(StackFrame frame) {
+        frame.push((int) 4);
+    }
+
+    public static void iconst_5(StackFrame frame) {
+        frame.push((int) 5);
+    }
+
+    public static void iconst_m1(StackFrame frame) {
+        frame.push((int) -1);
+    }
+    public static void iload(int index,StackFrame frame){
+        frame.push(frame.getLocalAt(index));
+    }
+    public static void iload_0(StackFrame frame){
+        frame.push(frame.getLocalAt(0));
+    } 
+    public static void iload_1(StackFrame frame){
+        frame.push(frame.getLocalAt(1));
+    } 
+    public static void iload_2(StackFrame frame){
+        frame.push(frame.getLocalAt(2));
+    } 
+    public static void iload_3(StackFrame frame){
+        frame.push(frame.getLocalAt(3));
+    } 
+    public static void istore(int index,StackFrame frame){
+        frame.setLocalAt(frame.pop(),index);
+    } 
+    public static void istore_0(StackFrame frame){
+        frame.setLocalAt(frame.pop(),0);
+    } 
+    public static void istore_1(StackFrame frame){
+        frame.setLocalAt(frame.pop(),1);
+    } 
+    public static void istore_2(StackFrame frame){
+        frame.setLocalAt(frame.pop(),2);
+    } 
+    public static void istore_3(StackFrame frame){
+        frame.setLocalAt(frame.pop(),3);
+    } 
+
+
 }
