934936bdffefb5f5e12bc0f95c27b7a851d858bc
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-12-09 13:19:44 +0200

    returning back to the jvm implementation

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,34 +1,31 @@
 const stompClient = new StompJs.Client({
-    brokerURL: 'ws://localhost:8080/gs-guide-websocket'
+    brokerURL: 'ws://localhost:8080/gs-guide-websocket',
+    onWebSocketError: (error) => {
+        console.error('Error with websocket', error);
+    },
+    onStompError: (frame) => {
+        console.error('Broker reported error: ' + frame.headers['message']);
+        console.error('Additional details: ' + frame.body);
+    }
 });
 
 stompClient.onConnect = (frame) => {
     setConnected(true);
     console.log('Connected: ' + frame);
-    stompClient.subscribe('/topic/greetings', (greeting) => {
-        showGreeting(JSON.parse(greeting.body).content);
+    stompClient.subscribe('/topic/stackFrames', (stackFrameMessage) => {
+        showStackFrame(JSON.parse(stackFrameMessage.body));
     });
-};
-
-stompClient.onWebSocketError = (error) => {
-    console.error('Error with websocket', error);
-};
-
-stompClient.onStompError = (frame) => {
-    console.error('Broker reported error: ' + frame.headers['message']);
-    console.error('Additional details: ' + frame.body);
 };
 
 function setConnected(connected) {
     $("#connect").prop("disabled", connected);
     $("#disconnect").prop("disabled", !connected);
     if (connected) {
-        $("#conversation").show();
+        $("#stackFrameContainer").show();
+    } else {
+        $("#stackFrameContainer").hide();
     }
-    else {
-        $("#conversation").hide();
-    }
-    $("#greetings").html("");
+    $("#stackFrameBody").html("");
 }
 
 function connect() {
@@ -41,20 +38,46 @@ function disconnect() {
     console.log("Disconnected");
 }
 
-function sendName() {
+function sendStackFrameRequest() {
+    // This could be a request to the server to send a StackFrame
     stompClient.publish({
-        destination: "/app/hello",
-        body: JSON.stringify({'name': $("#name").val()})
+        destination: "/app/getStackFrame",
+        body: JSON.stringify({}) // Adjust the body if necessary
     });
 }
 
-function showGreeting(message) {
-    $("#greetings").append("<tr><td>" + message + "</td></tr>");
+function showStackFrame(stackFrame) {
+    // Clear the current content in the table
+    $("#stackFrameBody").empty();
+
+    // Display local variables
+    var localVarsHtml = stackFrame.localVars.map(function (localVar) {
+        return `<td>${localVar}</td>`;
+    }).join('');
+
+    // Display operand stack
+    var operandStackHtml = stackFrame.operandStack.map(function (operand) {
+        return `<td>${operand}</td>`;
+    }).join('');
+
+    // Display frame data
+    var frameDataHtml = stackFrame.frameData.map(function (frameData) {
+        return `<td>${frameData}</td>`;
+    }).join('');
+
+    // Append the row with the data
+    $('#stackFrameBody').append(
+        `<tr>
+            ${localVarsHtml}
+            ${operandStackHtml}
+            ${frameDataHtml}
+        </tr>`
+    );
 }
 
 $(function () {
     $("form").on('submit', (e) => e.preventDefault());
-    $( "#connect" ).click(() => connect());
-    $( "#disconnect" ).click(() => disconnect());
-    $( "#send" ).click(() => sendName());
+    $("#connect").click(() => connect());
+    $("#disconnect").click(() => disconnect());
+    $("#send").click(() => sendStackFrameRequest());
 });
